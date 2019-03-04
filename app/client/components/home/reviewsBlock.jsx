f2856e41e9faa7f3cbb1bfef5050adaaad28228e
sikorsky_dmitry-at-062869851462,aE9isGC/ <sikorskiy7777@gmail.com>
2019-03-04 18:46:24 +0200

    need fix stars

---
+++ app/client/components/home/reviewsBlock.jsx
@@ -0,0 +1,54 @@
+
+'use strict';
+
+import React, {PureComponent} from 'react'
+import { Link } from 'react-router-dom'
+
+import Stars from './stars'
+
+const reviewsListArr = [
+    {img: '//robohash.org/d4c86ee2c4301b366e4d806b16113e35.png?size=32x32', rating: '4', content: 'Fevi uf jav mena je pauce zeda po atadrul kon budtupwe fehlieba dukreri huchahpag corerabo miene aptif. Okuruzo akbebcin kunobag sazhun jusorin guhbor hunopifo nugmob ki su apabuha ga jekbejvo fig nuep. Zi gub duki nahne zomubha zavahik we ru penmuit madunwid se fa infolmar zas wenjimje fojof zine necvez.'},
+    {img: '//robohash.org/a1d9d0b81e3ea5bf481091bb115b93e9.png?size=32x32', rating: '5', content: 'Fevi uf jav mena je pauce zeda po atadrul kon budtupwe fehlieba dukreri huchahpag corerabo miene aptif. Okuruzo akbebcin kunobag sazhun jusorin guhbor hunopifo nugmob ki su apabuha ga jekbejvo fig nuep. Zi gub duki nahne zomubha zavahik we ru penmuit madunwid se fa infolmar zas wenjimje fojof zine necvez.'},
+    {img: '//robohash.org/ac6f475a26838a9b49169ff19344d98b.png?size=32x32', rating: '2', content: 'Fonvef atufazfok muodil kaj mirzif be ju maz no gim tob leir naig ka it idhib. Wifat mifhatwih wuce cuw evege kipfihim ca zigikras ku ukcem nihduhad rifa. Ca bi bizid vuk mamnifwul cukibhuz irikut sal idelokhug duhu wol nohresu tausudo zoc kevugiew booco ijzi tesufhag. Orkunor ejius he ce er orfi cutef higoke sipgov vozuzwe agci ef bausead zepez idtozoz.'},
+];
+
+class ReviewsBlock extends PureComponent{
+
+    render(){
+        const reviewsList = reviewsListArr.map((item, i) =>
+            <li className="home__block__item" key={i} >
+                <Link to={`reviews/${i}`} className='link'>
+                    <img src={item.img} alt="" width='40' />
+                    <div className="home__block__content">
+
+                        {<Stars rating={item.rating} />}
+
+
+                        <p className='text'>{item.content}</p>
+                    </div>
+                </Link>
+            </li>
+        );
+
+        return(
+            <div className="home__block list reviews">
+                <div className="home__block__header clearfix">
+                    <div className="home__block__info">
+                        <span className="home__block__count">{reviewsListArr.length}</span>
+                        <span className="home__block__sbt">Pending Reviews</span>
+                    </div>
+                    <div className="home__block__icon">
+                        <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" width="54">
+                            <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM18 14H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z"/>
+                        </svg>
+                    </div>
+                </div>
+                <ul className="home__block__list">
+                    {reviewsList}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default ReviewsBlock
--- app/client/containers/home/home.jsx
+++ app/client/containers/home/home.jsx
@@ -4,6 +4,11 @@ import './_home.sass'
 
 import React, {Component} from 'react'
 
+import RevenueBlock from './../../components/home/revenueBlock'
+import NewOrdersBlock from './../../components/home/newOrdersBlock'
+import PendingOrdersBlock from './../../components/home/pendingOrdersBlock'
+import ReviewsBlock from './../../components/home/ReviewsBlock'
+
 class Home extends Component{
 
     render(){
@@ -11,34 +16,36 @@ class Home extends Component{
             <div className='home__wrap clearfix'>
 
                 <div className="home__inner">
-                    <div className="home__block half price">
-                        <div className="home__block__info">
-                            <span className="home__block__count">7&nbsp;430&nbsp;$</span>
-                            <span className="home__block__sbt">Monthly Revenue</span>
-                        </div>
-                        <div className="home__block__icon">
-                            <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" width='54'>
-                                <path d="M11.8 10.9c-2.27-.59-3-1.2-3-2.15 0-1.09 1.01-1.85 2.7-1.85 1.78 0 2.44.85 2.5 2.1h2.21c-.07-1.72-1.12-3.3-3.21-3.81V3h-3v2.16c-1.94.42-3.5 1.68-3.5 3.61 0 2.31 1.91 3.46 4.7 4.13 2.5.6 3 1.48 3 2.41 0 .69-.49 1.79-2.7 1.79-2.06 0-2.87-.92-2.98-2.1h-2.2c.12 2.19 1.76 3.42 3.68 3.83V21h3v-2.15c1.95-.37 3.5-1.5 3.5-3.55 0-2.84-2.43-3.81-4.7-4.4z" />
-                            </svg>
+                    <div className="flex-container">
+                        <RevenueBlock />
+                        <NewOrdersBlock />
+
+                    </div>
+
+                    <div className="home__block standard">
+                        <h2 className="home__block__title">Welcome to react-admin demo</h2>
+                        <p className="home__block__content">This is the admin of an imaginary poster shop. Fell free to explore and modify the data - it's local to your computer, and will reset each time you reload.</p>
+                        <div className="home__block__footer">
+                            <a href="" role='button' className="link">
+                                <svg width='24' focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+                                    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+                                </svg>
+                                <span>react-admin site</span>
+                            </a>
+                            <a href="" role='button' className="link">
+                                <svg width='24' focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+                                    <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z" />
+                                </svg>
+                                <span>Source for this demo</span>
+                            </a>
                         </div>
                     </div>
-                    <div className="home__block half orders">
-                        <div className="home__block__info">
-                            <span className="home__block__count">17</span>
-                            <span className="home__block__sbt">New Orders</span>
-                        </div>
-                        <div className="home__block__icon">
-                            <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" width='54'>
-                                <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
-                            </svg>
-                        </div>
-                    </div>
+                    <PendingOrdersBlock />
                 </div>
 
                 <div className="home__inner">
-
+                    <ReviewsBlock />
                 </div>
-
 
             </div>
         )
