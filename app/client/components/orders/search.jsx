20fabbf44eb49dd8d3a87d4be53192551f9591e8
Dmitriy Sikorskiy <sikorskiy7777@gmail.com>
2019-03-06 22:53:14 +0200

    fixed toggling class search input

--- app/client/components/orders/search.jsx
+++ app/client/components/orders/search.jsx
@@ -7,28 +7,33 @@ import ReactDOM from 'react-dom'
 
 class OrdersSearch extends PureComponent{
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
             fieldIsActive: false,
             fieldValue: ''
-        }
+        };
+
+        this.getWrapRef = React.createRef();
+        this.getInputRef = React.createRef();
+
     }
 
     componentDidMount(){
 
+        const wrap = this.getWrapRef.current;
+        const input = this.getInputRef.current;
+
         document.body.addEventListener('click', function (e) {
 
-            const el = document.getElementById('ordersSearch');
-            const label = document.querySelector('.orders__search__wrap label');
-            const elWrap = document.getElementsByClassName('orders__search__wrap')[0];
+            if(e.target == input){
+                wrap.classList.add('active');
 
-            if(e.target != el && e.target != label && !el.value)
-                elWrap.classList.remove('active');
+            }else if(!input.value)
+                wrap.classList.remove('active');
 
         });
-
 
     }
 
@@ -36,32 +41,16 @@ class OrdersSearch extends PureComponent{
         this.setState({fieldValue: e.target.value.toLowerCase()});
     }
 
-    handleFieldState(){
-        this.setState({fieldIsActive: true});
-    }
-
-    getWrapRef(node){
-
-
-        const testWrap = ReactDOM.findDOMNode(this._wrap);
-
-        console.log(testWrap);
-
-        return this._wrap = node;
-
-    }
-
     render() {
         return(
-            <div className={this.state.fieldIsActive ? "orders__search__wrap clearfix active" : "orders__search__wrap clearfix"} ref={this.getWrapRef}  >
+            <div className={this.state.fieldIsActive ? "orders__search__wrap clearfix active" : "orders__search__wrap clearfix"}
+                 ref={this.getWrapRef}
+            >
                 <label>Search</label>
                 <input type="text" id='ordersSearch'
-                       onClick={this.handleFieldState.bind(this)}
-                       //onMouseOver={this.handleFieldState.bind(this)}
-                       //onMouseOut={this.handleFieldState.bind(this)}
-                       //onFocus={this.handleFieldState.bind(this)}
                        onChange={this.search.bind(this)}
                        className='orders__search__field'
+                       ref={this.getInputRef}
                 />
             </div>
         )
