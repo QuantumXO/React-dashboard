8a5564a84c3311b6b3fe605b9a48a2c84ed412a6
Sikorskiy Dmitriy <sikorskiy7777@gmail.com>
2019-03-19 19:13:58 +0200

    upd

--- app/client/index.jsx
+++ app/client/index.jsx
@@ -7,20 +7,23 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
 // React router
 import { BrowserRouter } from 'react-router-dom'
+import persistStore from 'redux-phoenix' // localstorage store
 
 import configureStore from './store/configureStore'
 import App from "./containers/app";
 
 const store = configureStore();
 
-ReactDOM.hydrate(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
-);
+persistStore(store).then(store => {
+    ReactDOM.hydrate(
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>,
+        document.getElementById('root')
+    )
+});
 
 if (module.hot) {
     module.hot.accept();
--- app/client/store/configureStore.jsx
+++ app/client/store/configureStore.jsx
@@ -1,8 +1,9 @@
 'use strict';
 
-import { createStore, applyMiddleware } from 'redux'
-import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { autoRehydrate } from 'redux-phoenix'
+import { createStore, applyMiddleware, compose } from 'redux'
 
 import rootReducer from './../reducers/rootReducer'
 
@@ -11,7 +12,11 @@ export default function configureStore() {
 
     const store = createStore(
         rootReducer,
-        applyMiddleware(thunk, logger));
+        compose(
+            applyMiddleware(thunk, logger),
+            autoRehydrate
+        )
+    );
 
     return store;
 }
